8fa9034068fd03a6ebc825facb7c64809459eb2d
NguyenNgocQuangMinh <quangminh0512@icloud.com>
2023-11-04 02:07:37 +0700

    format on save

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,15 @@
-from django.db import models
 from django.conf import settings
-from django.contrib.contenttypes.fields import GenericRelation, GenericForeignKey
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
+from django.db import models
 
 
 class Tag(models.Model):
-  value = models.TextField(max_length=100)
+    value = models.TextField(max_length=100)
 
-  def __str__(self):
-    return self.value
+    def __str__(self):
+        return self.value
+
 
 class AuthorProfile(models.Model):
     user = models.OneToOneField(
@@ -19,27 +20,31 @@ class AuthorProfile(models.Model):
     def __str__(self):
         return f"{self.__class__.__name__} object for {self.user}"
 
+
 class Comment(models.Model):
-  creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-  content = models.TextField()
-  content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-  object_id = models.TextField(db_index=True)
-  content_object = GenericForeignKey('content_type', 'object_id',)
-  created_at = models.DateTimeField(auto_now_add=True, db_index=True)
-  modified_at = models.DateTimeField(auto_now=True)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.TextField(db_index=True)
+    content_object = GenericForeignKey(
+        "content_type",
+        "object_id",
+    )
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
 
 class Post(models.Model):
-  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
-  created_by = models.DateTimeField(auto_now_add=True)
-  modified_at = models.DateTimeField(auto_now=True)
-  published_at = models.DateTimeField(blank=True, null=True, db_index=True)
-  title = models.TextField(max_length=100)
-  slug = models.SlugField()
-  summary = models.TextField(max_length=500)
-  content = models.TextField()
-  tags = models.ManyToManyField(Tag, related_name="posts")
-  comment = GenericRelation(Comment)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    created_by = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(blank=True, null=True, db_index=True)
+    title = models.TextField(max_length=100)
+    slug = models.SlugField()
+    summary = models.TextField(max_length=500)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="posts")
+    comment = GenericRelation(Comment)
 
-  def __str__(self):
-    return self.title
-
+    def __str__(self):
+        return self.title
