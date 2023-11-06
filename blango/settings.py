a84e9e4e6d50e8d5e07e0952c0671a3f5794f976
Nguyễn Minh <quangminh05122001@gmail.com>
2023-11-06 20:27:43 +0000

    Finish First-Party Django REST API

--- blango/settings.py
+++ blango/settings.py
@@ -29,19 +29,19 @@ class Dev(Configuration):
     # SECURITY WARNING don't run with debug turned on in production!
     DEBUG = values.BooleanValue(True)
 
-    # ALLOWED_HOSTS = values.ListValue(["localhost", "0.0.0.0", ".codio.io"])
-    #
-    # X_FRAME_OPTIONS = (
-    #     "ALLOW-FROM " + os.environ.get("CODIO_HOSTNAME") + "-8000.codio.io"
-    # )
-    # CSRF_COOKIE_SAMESITE = None
-    # CSRF_TRUSTED_ORIGINS = [
-    #     "https://" + os.environ.get("CODIO_HOSTNAME") + "-8000.codio.io"
-    # ]
-    # CSRF_COOKIE_SECURE = True
-    # SESSION_COOKIE_SECURE = True
-    # CSRF_COOKIE_SAMESITE = "None"
-    # SESSION_COOKIE_SAMESITE = "None"
+    ALLOWED_HOSTS = values.ListValue(["localhost", "0.0.0.0", ".codio.io"])
+    
+    X_FRAME_OPTIONS = (
+        "ALLOW-FROM " + os.environ.get("CODIO_HOSTNAME") + "-8000.codio.io"
+    )
+    CSRF_COOKIE_SAMESITE = None
+    CSRF_TRUSTED_ORIGINS = [
+        "https://" + os.environ.get("CODIO_HOSTNAME") + "-8000.codio.io"
+    ]
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SAMESITE = "None"
+    SESSION_COOKIE_SAMESITE = "None"
 
     # Application definition
     AUTH_USER_MODEL = "blango_auth.User"
